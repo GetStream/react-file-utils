590f2c5a04813a2e649851fe796794f23f9f20e8
Jelte Fennema <github-tech@jeltef.nl>
2019-01-29 17:27:19 +0100

    Update configs

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,14 @@ import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
 import external from 'rollup-plugin-peer-deps-external'
 import postcss from 'rollup-plugin-postcss'
-import resolve from 'rollup-plugin-node-resolve'
 import url from 'rollup-plugin-url'
-import svgr from '@svgr/rollup'
+import replace from 'rollup-plugin-replace';
+
+import atImport from 'postcss-easy-import';
+import cssnext from 'postcss-cssnext';
+import nested from 'postcss-nested';
+import colorFunction from 'postcss-color-function';
+import vars from 'postcss-simple-vars';
 
 import pkg from './package.json'
 
@@ -22,18 +27,50 @@ export default {
       sourcemap: true
     }
   ],
+  external: [
+    'react-images',
+    'lodash',
+    'react-dropzone',
+    'immutable',
+    'url-parse',
+    'prop-types',
+    '@fortawesome/react-fontawesome',
+    '@fortawesome/free-regular-svg-icons',
+    '@babel/runtime/regenerator',
+    '@babel/runtime/helpers/asyncToGenerator',
+    '@babel/runtime/helpers/objectWithoutProperties',
+    '@babel/runtime/helpers/toConsumableArray',
+    '@babel/runtime/helpers/objectSpread',
+    '@babel/runtime/helpers/extends',
+    '@babel/runtime/helpers/defineProperty',
+    '@babel/runtime/helpers/assertThisInitialized',
+    '@babel/runtime/helpers/inherits',
+    '@babel/runtime/helpers/getPrototypeOf',
+    '@babel/runtime/helpers/possibleConstructorReturn',
+    '@babel/runtime/helpers/createClass',
+    '@babel/runtime/helpers/classCallCheck',
+  ],
   plugins: [
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
     external(),
     postcss({
-      modules: true
+      plugins: [
+        atImport(),
+        nested(),
+        //vars({ variables }),
+        colorFunction(),
+        cssnext(),
+      ],
+      modules: false,
+      extract: true,
     }),
     url(),
-    svgr(),
     babel({
+      runtimeHelpers: true,
       exclude: 'node_modules/**',
-      plugins: [ 'external-helpers' ]
     }),
-    resolve(),
     commonjs()
   ]
 }
