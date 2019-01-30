16327a5d667a2616259567304c3e3eb7d1d3b2eb
Jelte Fennema <github-tech@jeltef.nl>
2019-01-30 11:24:17 +0100

    Copy over files

---
+++ src/types.js
@@ -0,0 +1,24 @@
+// @flow
+
+import * as React from 'react';
+
+export type UploadState = 'uploading' | 'finished' | 'failed';
+
+export type FileLike = Blob | File;
+
+export type UploadInfo = {|
+  id: string,
+  url?: string,
+  state: UploadState,
+|};
+
+export type FileUpload = {|
+  ...UploadInfo,
+  file: File,
+|};
+
+export type ImageUpload = {|
+  ...UploadInfo,
+  file: Blob | File,
+  previewUri?: string,
+|};
---
+++ src/utils.js
@@ -0,0 +1,92 @@
+// @flow
+import * as React from 'react';
+import type { FileLike } from './types';
+// import type { UserResponse } from 'getstream';
+
+// https://stackoverflow.com/a/6860916/2570866
+export function generateRandomId() {
+  // prettier-ignore
+  return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+}
+
+function S4() {
+  return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+}
+
+export function dataTransferItemsHaveFiles(
+  items: ?(DataTransferItem[]),
+): boolean {
+  if (!items || !items.length) {
+    return false;
+  }
+  for (const item of items) {
+    if (item.kind === 'file' || item.type === 'text/html') {
+      return true;
+    }
+  }
+  return false;
+}
+
+export async function dataTransferItemsToFiles(
+  items: ?(DataTransferItem[]),
+): Promise<FileLike[]> {
+  if (!items || !items.length) {
+    return [];
+  }
+  const fileLikes = [];
+  const blobPromises = [];
+  for (const item of items) {
+    if (item.kind === 'file') {
+      const file = item.getAsFile();
+      if (file) {
+        fileLikes.push(file);
+      }
+    }
+  }
+  // If there are files inside the DataTransferItem prefer those
+  if (fileLikes.length) {
+    return fileLikes;
+  }
+
+  // Otherwise extract images from html
+  const parser = new DOMParser();
+  for (const item of items) {
+    if (item.type === 'text/html') {
+      blobPromises.push(
+        new Promise((accept) => {
+          item.getAsString(async (s) => {
+            const doc = parser.parseFromString(s, 'text/html');
+            const imageTags = doc.getElementsByTagName('img');
+
+            const imagePromises = [];
+            for (const tag of imageTags) {
+              if (!tag.src) {
+                continue;
+              }
+              imagePromises.push(
+                (async () => {
+                  let res;
+                  try {
+                    res = await fetch(tag.src);
+                  } catch (e) {
+                    return;
+                  }
+                  const contentType =
+                    res.headers.get('Content-type') ||
+                    'application/octet-stream';
+                  const buf = await res.arrayBuffer();
+                  const blob = new Blob([buf], { type: contentType });
+                  fileLikes.push(blob);
+                })(),
+              );
+            }
+            await Promise.all(imagePromises);
+            accept();
+          });
+        }),
+      );
+    }
+  }
+  await Promise.all(blobPromises);
+  return fileLikes;
+}
