3686959d0f82c4ed030261be6384ea4e3a340652
Anton Arnautov <arnautov.anton@gmail.com>
2022-06-08 02:31:55 +0200

    refactor(FilePreviewer): use optional chaining

--- src/components/FilePreviewer.tsx
+++ src/components/FilePreviewer.tsx
@@ -22,55 +22,52 @@ export const FilePreviewer = ({
 }: FilePreviewerProps) => (
   <div className="rfu-file-previewer">
     <ol>
-      {uploads &&
-        uploads.map((upload) => (
-          <li
-            key={upload.id}
-            className={`rfu-file-previewer__file ${
-              upload.state === 'uploading'
-                ? 'rfu-file-previewer__file--uploading'
-                : ''
-            } ${
-              upload.state === 'failed'
-                ? 'rfu-file-previewer__file--failed'
-                : ''
-            }`}
-          >
-            <FileIcon mimeType={upload.file.type} filename={upload.file.name} />
+      {uploads?.map((upload) => (
+        <li
+          key={upload.id}
+          className={`rfu-file-previewer__file ${
+            upload.state === 'uploading'
+              ? 'rfu-file-previewer__file--uploading'
+              : ''
+          } ${
+            upload.state === 'failed' ? 'rfu-file-previewer__file--failed' : ''
+          }`}
+        >
+          <FileIcon mimeType={upload.file.type} filename={upload.file.name} />
 
-            <a href={upload.url} download>
-              {upload.file.name}
-              {upload.state === 'failed' && (
-                <React.Fragment>
-                  <div
-                    className="rfu-file-previewer__failed"
-                    onClick={handleRetry && (() => handleRetry(upload.id))}
-                  >
-                    failed
-                  </div>
-                  <div
-                    className="rfu-file-previewer__retry"
-                    onClick={handleRetry && (() => handleRetry(upload.id))}
-                  >
-                    retry
-                  </div>
-                </React.Fragment>
-              )}
-            </a>
-
-            <span
-              className="rfu-file-previewer__close-button"
-              onClick={handleRemove && (() => handleRemove(upload.id))}
-            >
-              ✘
-            </span>
-            {upload.state === 'uploading' && (
-              <div className="rfu-file-previewer__loading-indicator">
-                <LoadingIndicator />
-              </div>
+          <a href={upload.url} download>
+            {upload.file.name}
+            {upload.state === 'failed' && (
+              <>
+                <div
+                  className="rfu-file-previewer__failed"
+                  onClick={() => handleRetry?.(upload.id)}
+                >
+                  failed
+                </div>
+                <div
+                  className="rfu-file-previewer__retry"
+                  onClick={() => handleRetry?.(upload.id)}
+                >
+                  retry
+                </div>
+              </>
             )}
-          </li>
-        ))}
+          </a>
+
+          <span
+            className="rfu-file-previewer__close-button"
+            onClick={handleRemove && (() => handleRemove(upload.id))}
+          >
+            ✘
+          </span>
+          {upload.state === 'uploading' && (
+            <div className="rfu-file-previewer__loading-indicator">
+              <LoadingIndicator />
+            </div>
+          )}
+        </li>
+      ))}
     </ol>
   </div>
 );
