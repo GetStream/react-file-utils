91cf20291a4bd2b90cfc091531f073902d2f2b0d
Anton Arnautov <43254280+arnautov-anton@users.noreply.github.com>
2022-06-08 02:33:03 +0200

    refactor: general component refactor (icons and types) (#62)
    * refactor: ImagePreviewer (icon + general refactor)
    
    * refactor: Thumbnail, IconButton (icon + types)
    
    * refactor: add missing type to buttons
    
    * refactor(FilePreviewer): use optional chaining

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -1,17 +1,19 @@
-import React, { PropsWithChildren } from 'react';
+import React, { PropsWithChildren, MouseEventHandler } from 'react';
 
 export type IconButtonProps = {
-  onClick?: (e: React.SyntheticEvent) => void;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
 };
 
 /**
- * This is simply a button wrapper, add's a div with `role="button"` and a onClick
+ * This is simply a button wrapper, adds a div with `role="button"` and a onClick
  */
 export const IconButton = ({
   onClick,
   children,
 }: PropsWithChildren<IconButtonProps>) => (
   <button
+    type="button"
+    data-testid="cancel-upload-button"
     aria-label="Cancel upload"
     className="rfu-icon-button"
     onClick={onClick}
