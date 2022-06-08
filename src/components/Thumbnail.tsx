1d09948c20a6b75894df7dbfce6429c698334bd2
Anton Arnautov <43254280+arnautov-anton@users.noreply.github.com>
2022-06-08 02:49:51 +0200

    v1.1.12 (#65)
    * refactor: general component refactor (icons and types) (#62)
    
    * refactor: ImagePreviewer (icon + general refactor)
    
    * refactor: Thumbnail, IconButton (icon + types)
    
    * refactor: add missing type to buttons
    
    * refactor(FilePreviewer): use optional chaining
    
    * fix: add type to buttons (#62)

--- src/components/Thumbnail.tsx
+++ src/components/Thumbnail.tsx
@@ -1,27 +1,27 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, MouseEventHandler } from 'react';
 
 import { IconButton } from './IconButton';
 import { FilePlaceholder } from './FilePlaceholder';
+import { CloseIcon } from './CloseIcon';
 
 export type ThumbnailProps = {
-  handleClose?: (id?: string) => void;
+  handleClose?: MouseEventHandler<HTMLButtonElement>;
   size?: number;
   image: string;
   alt?: string;
   id?: string;
 };
 
-const svg =
-  '<svg width="28" height="28" viewBox="0 0 28 28" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><path d="M465 5c5.53 0 10 4.47 10 10s-4.47 10-10 10-10-4.47-10-10 4.47-10 10-10zm3.59 5L465 13.59 461.41 10 460 11.41l3.59 3.59-3.59 3.59 1.41 1.41 3.59-3.59 3.59 3.59 1.41-1.41-3.59-3.59 3.59-3.59-1.41-1.41z" id="b"/><filter x="-30%" y="-30%" width="160%" height="160%" filterUnits="objectBoundingBox" id="a"><feOffset in="SourceAlpha" result="shadowOffsetOuter1"/><feGaussianBlur stdDeviation="2" in="shadowOffsetOuter1" result="shadowBlurOuter1"/><feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.5 0" in="shadowBlurOuter1"/></filter></defs><g transform="translate(-451 -1)" fill-rule="nonzero" fill="none"><use fill="#000" filter="url(#a)" xlink:href="#b"/><use fill="#FFF" fill-rule="evenodd" xlink:href="#b"/></g></svg>';
-
 export const Thumbnail = ({
-  id,
   image,
   size = 100,
   handleClose,
   alt,
 }: ThumbnailProps) => {
-  const onClose = useCallback(() => handleClose?.(id), [id, handleClose]);
+  const onClose: MouseEventHandler<HTMLButtonElement> = useCallback(
+    (event) => handleClose?.(event),
+    [handleClose],
+  );
 
   return (
     <div
@@ -31,7 +31,7 @@ export const Thumbnail = ({
       <div className="rfu-thumbnail__overlay">
         {handleClose ? (
           <IconButton onClick={onClose}>
-            <div dangerouslySetInnerHTML={{ __html: svg }} />
+            <CloseIcon />
           </IconButton>
         ) : null}
       </div>
